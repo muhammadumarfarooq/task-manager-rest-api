9c656866265ba22d0870fac34fe31f6a8fd3c9d0
Muhammad Umar Farooq <develop.umar@gmail.com>
2023-06-07 19:55:12 +0500

    delete task and user

--- src/index.js
+++ src/index.js
@@ -44,6 +44,22 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+
+    } catch (e) {
+        console.log(e);
+        res.status(400).send(e);
+    }
+});
+
 app.post('/task', async (req, res) => {
     try {
         const task = new Task(req.body);
@@ -77,6 +93,21 @@ app.patch('/tasks/:id', async (req, res) => {
         res.status(400).send(e);
     }
 })
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
 
 
 app.listen(port, () => {
