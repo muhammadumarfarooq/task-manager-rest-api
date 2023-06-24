c13b5189a72a796598d6b6ce2085acbaf9a1fa7d
Muhammad Umar Farooq <develop.umar@gmail.com>
2023-06-24 17:07:42 +0500

    define the auth middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require("../models/user");
+const auth = require("../middleware/auth");
 
 const router = express.Router();
 
@@ -15,7 +16,7 @@ router.post('/user', async (req, res) => {
     }
 });
 
-router.get('/users', async (req, res) => {
+router.get('/users', auth, async (req, res) => {
     try {
         const users = await User.find({});
         res.send(users);
